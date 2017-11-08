d5a71b2335b9b5d2985a1c3712c5cee8090d315d
kj <konrad.jahn.84@gmail.com>
2017-11-08 14:55:07 +0100

    new functionalty, with kinks

--- script.js
+++ script.js
@@ -3,26 +3,39 @@ var time={
 	t:1500, //current clock time
 	work:1500, //work interval
 	rest:300, //rest interval
-	isOn: false //clock runnung boolean
+	isOn: false, //clock runnung boolean
+	reset: true, //reset mode active?
+	mode: "work" //which cycle is active
 }; 
-var clockCode; //time.tout code for clock
-// var onOff=false; //time.tr running boolean
+var clockCode; //time.out code for clock
 
 
 //define functions
-function runClock(){ //selfreferential clock function
-  if(time.t>1){
-    time.t--; 
-    displayClock();
-    //echo to console
-    clockCode=window.setTimeout(runClock,1000); //recursive call
-    return;
-  } else {
-  	time.t = 0;
-  	displayClock();
-  	alarm();
-  }
+function runClock(){ //recursive clock function
+	time.isOn=true; //flip controlling boolean
+	if(time.t>1){
+		time.t--; 
+		displayClock();
+    	//echo to console
+    	clockCode=window.setTimeout(runClock,1000); //recursive call
+    	return;
+	} else {
+		time.t = 0;
+		displayClock();
+		alarm();
+	}
 } 
+function stopClock(){ //kill running clock function
+	window.clearTimeout(clockCode); //stop clock function
+	time.isOn=false; //flip clock is on boolean
+}
+function resetClock(){ //reset the clock
+	stopClock();
+	time.reset = true;
+	time.t=time.work;
+	$('.digitalOn').removeClass('.digitalOn');
+	displayClock();
+}
 function fmt(x){ // convert time to string and add "0" for disp
  	if (x<10) {
  		return "0"+x.toString();
@@ -40,12 +53,16 @@ function displayInit(name){ //display work/rest init values
 	var m = time[name]/60;
 	$('#'+name+'Face').html(fmt(m));
 }
-
+function displayMode(){
+	$(".adjDiv").removeClass('digitalOn');
+	$("#"+time.mode+'Face').addClass('digitalOn');
+}
 function alarm(){ //placeholder alarm function
 	console.log("beep!");
+	if (time.mode == 'work'){
+
+	}
 }
-
-
 function adjInit(name,plusMinus){ //adjust init values of work/rest 
 	if (plusMinus == "+") {
 		if (time[name]<99*60) {
@@ -57,9 +74,14 @@ function adjInit(name,plusMinus){ //adjust init values of work/rest
 		} 
 	}
 	displayInit(name);
+	if (!time.onOff && time.reset && name == "work"){
+		time.t = time[name];
+		displayClock();
+	}
 }
 
-$("document").ready(function(){  //on DOM load
+//run stuff on DOM Load!!
+$("document").ready(function(){
 	//initial display
 	displayClock();
 	displayInit('work');
@@ -68,16 +90,19 @@ $("document").ready(function(){  //on DOM load
 	//start button behavior
 	$("#start").click(function(){
 		if (time.isOn) {
-			window.clearTimeout(clockCode);
-			$("#start").html('<i class="fa fa-play"></i>');
-			time.isOn=false;
+			stopClock();
+			$("#start").html('<i class="fa fa-play"></i>'); //change icon
+			$(".clockFace").removeClass('digitalOn');
 		} else {
-			setTimeout(runClock,1000);
-			$("#start").html('<i class="fa fa-pause"></i>');
-			time.isOn=true;
+			setTimeout(runClock,1000); //run clock after initial timeout
+			$("#start").html('<i class="fa fa-pause"></i>');  //change icon
+			$(".clockFace").addClass('digitalOn');
+			displayMode();
+			time.reset=false; //reset mode off
 		}
 	});
 	
+	//init button behavior
 	$("#workPlus").click(function(){
 		adjInit('work','+');
 	});
@@ -89,5 +114,10 @@ $("document").ready(function(){  //on DOM load
 	});
 	$("#restMinus").click(function(){
 		adjInit('rest','-');
+	});
+
+	//reset button behavior
+	$('#reset').click(function(){
+		resetClock();
 	});
 });
